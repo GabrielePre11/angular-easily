47a01896bf3386eff3c7be8a068e60e7e8dfbea9
Gabriele Prestano <gabryprestanoitaly@gmail.com>
2026-03-20 09:41:24 +0100

    feat(backend): initialize Express server with middleware configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//============ Configuration ============//
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const API_URL = process.env.API_URL || "api/v1";
+
+//============ Middlewares ============//
+app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    hsts: { maxAge: 31536000, includeSubDomains: false },
+  }),
+);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+
+//============ Server Start ============//
+app.listen(PORT, () => console.info(`Server running on port ${PORT}`));
